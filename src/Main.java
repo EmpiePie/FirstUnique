9d584150390a48d48e771ac24d08f38f4bb07444
EmpiePie <raoul.schwenke@gmail.com>
2023-01-30 19:15:45 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] A =  new int[6];
-        A[0] = 6;
-        A[1] = 4;
-        A[2] = 6;
-        A[3] = 7;
-        A[4] = 1;
-        A[5] = 7;
+        int[] A = {4, 5, 6, 6, 4, 7};
 
 
         System.out.println(Solution.solution(A));
--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,68 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Solution {
 
     public static int solution (int[] A) {
 
-         if(A.length > 100000) {
-
-            return -1;
+        Set<Integer> input = new LinkedHashSet<>();
+        Set<Integer> duplicates = new HashSet<>();
+        for (int x : A) {
+            if (!input.add(x)) {
+                duplicates.add(x);
+            }
         }
+        input.removeAll(duplicates);
+        return input.isEmpty() ? -1 : input.iterator().next();
 
-        ArrayList<Integer> list = new ArrayList<>();
-
-         for (int a = 0; a < A.length; a++) {
-
-             list.add(A[a]);
-         }
-
-         while (list.size() > 0) {
-
-             if (list.get(0) == list.get(1)) {
-
-                 list.remove(1);
-             }
-         }
-
-        return -1;
     }
+
+//    public static int solution (int[] A) {
+//
+//         if(A.length > 100000) {
+//
+//            return -1;
+//        }
+//
+//         else if (A.length == 1) {
+//
+//             return A[0];
+//         }
+//
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        for (int value : A) {
+//
+//            list.add(value);
+//        }
+//
+//         for (int i = 0; i < A.length; i++) {
+//
+//             for (int j = i + 1; j < A.length; j++) {
+//
+//                 if (A[i] == A[j]) {
+//
+//                     for (int k = 0; k < list.size(); k++) {
+//
+//                         if (list.get(k) == A[i]) {
+//
+//                             list.set(k, 0);
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//
+//         for (int l = 0; l < list.size(); l++) {
+//
+//             if (list.get(l) != 0) {
+//
+//                 return list.get(l);
+//             }
+//         }
+//
+//        return -1;
+//    }
 }
